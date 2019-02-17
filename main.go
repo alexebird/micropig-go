4dfacca44805f5a5c1a11caa6d245eb72804cd3f
Alex Bird <alexebird@gmail.com>
2019-02-17 00:24:01 -0800

    add waitForAsgStatus

--- main.go
+++ main.go
@@ -49,17 +49,18 @@ func main() {
 		region:   "sfo2",
 		size:     "s-1vcpu-1gb",
 		snapshot: "micropig-feb-16-2019-220616",
-		count:    2,
+		desired:  2,
+		min:      0,
+		max:      2,
 	}
-	drops, _ := d.createAsg(asgOpts)
-	spew.Dump(drops)
+	asg, _ := d.createAsg(asgOpts)
+	s(asg)
+	d.waitForAsgStatus(asg, "ok")
+	s(asg)
 }
 
-func (d *doClient) getSnapshotBySlug(slug string) (int, error) {
-	listOpts := &godo.ListOptions{
-		Page:    1,
-		PerPage: 200,
-	}
+func (d *doClient) getSnapshotIdBySlug(slug string) (int, error) {
+	listOpts := &godo.ListOptions{Page: 1, PerPage: 200}
 
 	snapshots, _, err := d.client.Snapshots.ListDroplet(d.ctx, listOpts)
 	if err != nil {
@@ -78,7 +79,6 @@ func (d *doClient) getSnapshotBySlug(slug string) (int, error) {
 			break
 		}
 	}
-	//spew.Dump(snapshot)
 
 	snapID, err := strconv.ParseInt(snapshot.ID, 0, 64)
 	if err != nil {
@@ -93,15 +93,17 @@ type createAsgOptions struct {
 	region   string
 	size     string
 	snapshot string
-	count    int
+	desired  int
+	min      int
+	max      int
 }
 
 type createDropletOptions struct {
-	name_prefix string
-	region      string
-	size        string
-	imageID     int
-	index       int
+	name    string
+	region  string
+	size    string
+	imageID int
+	tags    []string
 }
 
 func sshKeyIDs() []godo.DropletCreateSSHKey {
@@ -111,8 +113,27 @@ func sshKeyIDs() []godo.DropletCreateSSHKey {
 	}
 }
 
-func (d *doClient) createAsg(opts *createAsgOptions) ([]godo.Droplet, error) {
-	snapID, err := d.getSnapshotBySlug(opts.snapshot)
+type micropigAsg struct {
+	name     string
+	status   string
+	droplets []godo.Droplet
+	desired  int
+	min      int
+	max      int
+}
+
+func (d *doClient) createAsg(opts *createAsgOptions) (*micropigAsg, error) {
+	if opts.min > opts.desired || opts.min > opts.max || opts.desired > opts.max {
+		return nil, errors.New("min <= desired <= max")
+	}
+
+	t := time.Now()
+
+	// name-region-ts-index
+	asgName := fmt.Sprintf("%s-%s-%s", opts.name, opts.region, t.Format("jan_22006T15-04-05Z"))
+	asgTag := "asg:" + asgName
+
+	snapID, err := d.getSnapshotIdBySlug(opts.snapshot)
 	if err != nil {
 		return nil, err
 	}
@@ -120,13 +141,13 @@ func (d *doClient) createAsg(opts *createAsgOptions) ([]godo.Droplet, error) {
 	c := make(chan *godo.Droplet)
 
 	go func() {
-		for i := 0; i < opts.count; i++ {
+		for i := 0; i < opts.desired; i++ {
 			createDropletOps := &createDropletOptions{
-				name_prefix: opts.name,
-				region:      opts.region,
-				size:        opts.size,
-				imageID:     snapID,
-				index:       i,
+				name:    fmt.Sprintf("%s-%d", asgName, i),
+				region:  opts.region,
+				size:    opts.size,
+				imageID: snapID,
+				tags:    []string{"all", asgTag},
 			}
 			drop := d.mustCreateDroplet(createDropletOps)
 			c <- drop
@@ -135,37 +156,94 @@ func (d *doClient) createAsg(opts *createAsgOptions) ([]godo.Droplet, error) {
 
 	droplets := make([]godo.Droplet, 0)
 
-	for i := 0; i < opts.count; i++ {
+	// wait for the droplets to be created in parallel
+	for i := 0; i < opts.desired; i++ {
 		drop := <-c
 		droplets = append(droplets, *drop)
 	}
 
-	return droplets, nil
+	asg := &micropigAsg{
+		status:   "new",
+		name:     asgName,
+		droplets: droplets,
+		desired:  opts.desired,
+		min:      opts.min,
+		max:      opts.max,
+	}
+
+	return asg, nil
+}
+
+func (d *doClient) waitForAsgStatus(asg *micropigAsg, status string) error {
+	timeout := time.Duration(300) // 5m
+	c := make(chan bool)
+
+	go func() {
+		for {
+			d.updateAsgStatus(asg)
+			if asg.status == status {
+				break
+			}
+		}
+		c <- true
+	}()
+
+	select {
+	case <-c:
+		return nil
+	case <-time.After(timeout * time.Second):
+		return errors.New("timeout waiting for asg status=" + status)
+	}
+}
+
+func (d *doClient) updateAsgStatus(asg *micropigAsg) error {
+	listOpts := &godo.ListOptions{Page: 1, PerPage: 200}
+	droplets, _, err := d.client.Droplets.ListByTag(d.ctx, "asg:"+asg.name, listOpts)
+
+	if err != nil {
+		return err
+	}
+
+	allActive := true
+	asg.droplets = droplets
+	currentSize := len(asg.droplets)
+
+	for _, drop := range asg.droplets {
+		status := drop.Status
+		if status != "active" {
+			allActive = false
+			break
+		}
+	}
+
+	if currentSize == asg.desired {
+		if allActive {
+			asg.status = "ok"
+		} else {
+			asg.status = "not_active"
+		}
+	} else if currentSize < asg.desired {
+		asg.status = "under"
+	} else if currentSize > asg.desired {
+		asg.status = "over"
+	} else {
+		return errors.New("unknown problem with asg")
+	}
+
+	return nil
 }
 
 func (d *doClient) mustCreateDroplet(opts *createDropletOptions) *godo.Droplet {
-	t := time.Now()
-
-	// name_prefix-region-ts-index
-	name := fmt.Sprintf("%s-%s-%s-%d",
-		opts.name_prefix,
-		opts.region,
-		t.Format("jan_22006T15-04-05Z"),
-		opts.index,
-	)
-
 	createRequest := &godo.DropletCreateRequest{
-		Name:   name,
-		Region: opts.region,
-		Size:   opts.size,
-		Image: godo.DropletCreateImage{
-			ID: opts.imageID,
-		},
+		Name:              opts.name,
+		Region:            opts.region,
+		Size:              opts.size,
+		Image:             godo.DropletCreateImage{ID: opts.imageID},
 		SSHKeys:           sshKeyIDs(),
 		PrivateNetworking: true,
 		Monitoring:        true,
 		UserData:          "",
-		Tags:              []string{},
+		Tags:              opts.tags,
 	}
 
 	if d.dryRun {
